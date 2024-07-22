8e14cc8dd638c014a60a67afc02fee2de3d0dcbc
VeraBelugina <vryshanchik-cherry@mail.ru>
2024-07-22 23:18:54 +0300

    Add files via upload

---
+++ 5_Tuples&Lists.py
@@ -0,0 +1,7 @@
+immutable_var_ = 3, 9, 'Hi', True
+print(immutable_var_)
+# кортеж не изменяемый тип
+mutable_list = ['land', 'water', 'air', 'fire']
+print(mutable_list)
+mutable_list.append('food')
+print(mutable_list)
